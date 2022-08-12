1a072faa77c40a47aadd25f8cb69d1857ee22603
Fatih Altun <fatih.altun@pardus.org.tr>
2022-08-12 17:05:00 +0300

    Fix and major update ui, backend

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -57,11 +57,9 @@ class MainWindow:
 
         # Home
         self.lbl_home_path = UI("lbl_home_path")
-        self.lbl_home_dev = UI("lbl_home_dev")
         self.lbl_home_size = UI("lbl_home_size")
 
         # Root
-        self.lbl_root_dev = UI("lbl_root_dev")
         self.pb_root_usage = UI("pb_root_usage")
         self.lbl_root_free = UI("lbl_root_free")
         self.lbl_root_total = UI("lbl_root_total")
@@ -87,6 +85,9 @@ class MainWindow:
         self.dlg_lbl_total_gb = UI("dlg_lbl_total_gb")
         self.dlg_lbl_filesystem_type = UI("dlg_lbl_filesystem_type")
 
+        # Device Type Stack
+        self.popover_dt_stack = UI("popover_dt_stack")
+
     
     def defineVariables(self):
         self.mount_operation = Gio.MountOperation.new()
@@ -108,7 +109,7 @@ class MainWindow:
         mount_point = vl.get_mount().get_root().get_parse_name()
         file_info = DiskManager.get_file_info(mount_point)
 
-        self.dlg_lbl_name.set_label(vl.get_name())
+        self.dlg_lbl_name.set_markup("<b><big>{}</big></b>".format(vl.get_name()))
         self.dlg_lbl_model.set_label(dr.get_name())
 
         self.dlg_lbl_dev.set_label(file_info["device"])
@@ -118,8 +119,8 @@ class MainWindow:
         self.dlg_lbl_free_gb.set_label(f"{int(file_info['free_kb'])/1024/1024:.2f} GB (%{file_info['free_percent']*100:.2f})")
         self.dlg_lbl_total_gb.set_label(f"{int(file_info['total_kb'])/1024/1024:.2f} GB")
 
-        self.dlg_lbl_filesystem_type.set_label(DiskManager.get_filesystem_of_partition(file_info["device"])) 
-    
+        self.dlg_lbl_filesystem_type.set_label(DiskManager.get_filesystem_of_partition(file_info["device"]))
+
 
     def showVolumeSizes(self, row_volume):
         mount_point = row_volume._volume.get_mount().get_root().get_parse_name()
@@ -127,11 +128,11 @@ class MainWindow:
 
         # Show values on UI
         row_volume._lbl_volume_name.set_markup(
-            f'<b>{row_volume._volume.get_name()}</b> <span size="small" alpha="75%">{ mount_point }</span>')
+            f'<b>{row_volume._volume.get_name()}</b> <span size="small">( { mount_point } )</span>')
         row_volume._lbl_volume_size_info.set_markup(
             f'<span size="small"><b>{int(file_info["free_kb"])/1024/1024:.2f} GB</b> {tr("is free of")} {int(file_info["total_kb"])/1024/1024:.2f} GB</span>')
-        row_volume._lbl_volume_dev_directory.set_markup(
-            f'<span size="small" alpha="75%">{ file_info["device"] }</span>')
+        # row_volume._lbl_volume_dev_directory.set_markup(
+        #     f'<span size="small" alpha="75%">{ file_info["device"] }</span>')
         row_volume._pb_volume_size.set_fraction(file_info["usage_percent"])
 
         row_volume._btn_volume_settings.set_sensitive(True)
@@ -171,14 +172,13 @@ class MainWindow:
 
         # Volume infos
         lbl_volume_name = Gtk.Label.new()
-        lbl_volume_name.set_markup(
-            f'<b>{vl.get_name()}</b>')
+        lbl_volume_name.set_markup(f'<b>{vl.get_name()}</b>')
         lbl_volume_name.set_halign(Gtk.Align.START)
 
-        lbl_volume_dev_directory = Gtk.Label.new()
-        lbl_volume_dev_directory.set_markup(
-            f'<span size="small"> </span>')
-        lbl_volume_dev_directory.set_halign(Gtk.Align.START)
+        # lbl_volume_dev_directory = Gtk.Label.new()
+        # lbl_volume_dev_directory.set_markup(
+        #     f'<span size="small"> </span>')
+        # lbl_volume_dev_directory.set_halign(Gtk.Align.START)
 
         pb_volume_size = Gtk.ProgressBar.new()
         pb_volume_size.set_valign(Gtk.Align.CENTER)
@@ -191,30 +191,32 @@ class MainWindow:
 
         # Add widgets to box:
         box_volume_info.add(lbl_volume_name)
-        box_volume_info.add(lbl_volume_dev_directory)
+        # box_volume_info.add(lbl_volume_dev_directory)
         box_volume_info.add(pb_volume_size)
         box_volume_info.add(lbl_volume_size_info)        
 
         # Add Disk settings button
         btn_volume_settings = Gtk.MenuButton.new()
+        btn_volume_settings.set_image(Gtk.Image.new_from_icon_name("view-more-symbolic", Gtk.IconSize.LARGE_TOOLBAR  ))
         btn_volume_settings.set_relief(Gtk.ReliefStyle.NONE)
-        btn_volume_settings.set_valign(Gtk.Align.START)
+        btn_volume_settings.set_valign(Gtk.Align.CENTER)
         btn_volume_settings._volume = vl
         btn_volume_settings._lbl_volume_name = lbl_volume_name
         btn_volume_settings._lbl_volume_size_info = lbl_volume_size_info
         btn_volume_settings._pb_volume_size = pb_volume_size
-        btn_volume_settings._lbl_volume_dev_directory = lbl_volume_dev_directory
+        btn_volume_settings._is_removable = is_removable
+        # btn_volume_settings._lbl_volume_dev_directory = lbl_volume_dev_directory
+
         btn_volume_settings.connect("released", self.on_btn_volume_settings_clicked)
-        if is_removable:
-            btn_volume_settings.set_popover(self.popover_removable)
-        else:
-            btn_volume_settings.set_popover(self.popover_volume)
+
+        btn_volume_settings.set_popover(self.popover_volume)
         btn_volume_settings.set_sensitive(False)
 
         box_volume.add(img_volume)
         box_volume.pack_start(box_volume_info, True, True, 0)
-        box_volume.add(btn_volume_settings)
+        box_volume.pack_end(btn_volume_settings, False, True, 0)
         box_volume.props.margin = 7
+
 
         # Add to listbox
         listbox.prepend(box_volume)
@@ -224,7 +226,7 @@ class MainWindow:
         row._lbl_volume_name = lbl_volume_name
         row._lbl_volume_size_info = lbl_volume_size_info
         row._pb_volume_size = pb_volume_size
-        row._lbl_volume_dev_directory = lbl_volume_dev_directory
+        # row._lbl_volume_dev_directory = lbl_volume_dev_directory
         
         self.tryMountVolume(row)
 
@@ -232,12 +234,10 @@ class MainWindow:
     def addDisksToGUI(self):
         # Home:
         home_info = DiskManager.get_file_info(GLib.get_home_dir())
-        self.lbl_home_path.set_label(GLib.get_home_dir())
-        self.lbl_home_dev.set_label(home_info["device"])
+        self.lbl_home_path.set_markup("<small>( {} )</small>".format(GLib.get_home_dir()))
 
         # Root:
         root_info = DiskManager.get_file_info("/")
-        self.lbl_root_dev.set_label(root_info["device"])
         self.lbl_root_free.set_label(f"{int(root_info['free_kb'])/1024/1024:.2f} GB")
         self.lbl_root_total.set_label(f"{int(root_info['total_kb'])/1024/1024:.2f} GB")
         self.pb_root_usage.set_fraction( root_info["usage_percent"] )
@@ -330,30 +330,37 @@ class MainWindow:
             subprocess.run(["xdg-open", mount.get_root().get_parse_name()])
     
     def on_btn_volume_settings_clicked(self, btn):
+
+        self.popover_volume.set_relative_to(btn)
+        self.popover_volume.set_position(Gtk.PositionType.LEFT)
+
+        if btn._is_removable:
+            self.popover_dt_stack.set_visible_child_name("usb")
+        else:
+            self.popover_dt_stack.set_visible_child_name("disk")
+
+
         mount = btn._volume.get_mount()
         if mount == None:
             self.tryMountVolume(btn)
             return
 
         self.selected_volume = btn._volume
-        
-        self.popover_removable.set_sensitive(True)
+
+        # self.popover_removable.set_sensitive(True)
         self.popover_volume.set_sensitive(True)
-        
+
         mount_point = mount.get_root().get_parse_name()
         self.selected_volume_info = DiskManager.get_file_info(mount_point)
 
         self.cb_mount_on_startup.set_active(DiskManager.is_drive_automounted(self.selected_volume_info["device"]))
+
+        self.showDiskDetailsDialog(self.selected_volume)
+
     
     # Popover Menu Buttons:
     def on_cb_mount_on_startup_released(self, cb):
         DiskManager.set_automounted(self.selected_volume_info["device"], cb.get_active())
-    
-    def on_btn_volume_details_clicked(self, btn):
-        self.showDiskDetailsDialog(self.selected_volume)
-
-        self.dialog_disk_details.run()
-        self.dialog_disk_details.hide()
     
     def on_btn_format_removable_clicked(self, btn):
         mount_point = self.selected_volume.get_mount().get_root().get_parse_name()
@@ -368,3 +375,8 @@ class MainWindow:
     def on_mount_removed(self, volumemonitor, mount):
         self.addHardDisksToList()
         self.addRemovableDevicesToList()
+    def on_btn_volume_details_clicked(self, btn):
+        self.showDiskDetailsDialog(self.selected_volume)
+
+        self.dialog_disk_details.run()
+        self.dialog_disk_details.hide()
