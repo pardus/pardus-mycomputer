8762acfc59013be4a4729530d02ceefbec6d748e
sulincix <aliriza.keskin@pardus.org.tr>
2024-05-29 11:16:23 +0000

    fix kde wayland wrong icon issue

--- src/Main.py
+++ src/Main.py
@@ -10,6 +10,9 @@ from MainWindow import MainWindow
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, application_id="tr.org.pardus.mycomputer", flags=Gio.ApplicationFlags.NON_UNIQUE, **kwargs)
+
+        self.window = None
+        GLib.set_prgname("tr.org.pardus.mycomputer")
     
     def do_activate(self):
         self.window = MainWindow(self)
